51e69d1c3033d6d2d70997cb267d0c42ce475278
WesleygsDev <wes_legends@hotmail.com>
2022-08-02 06:25:51 -0300

    Visão do Briefing

---
+++ visualizar.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+ <?php
+    session_start();
+    if(!isset($_SESSION['id']))
+    {
+        echo "<script>window.location.href = 'register.php';</script>";
+    }
+?>
+<html lang="pt-br">
+<body>
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                
+
+                <?php include 'topbar.php';?>
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <?php include'viewBriefing.php';?>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- Footer -->
+            <footer class="sticky-footer">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Todos os Direitos Reservados 2022 | Briefingonline</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    
+
+    <!-- Bootstrap core JavaScript-->
+    <?php include 'footer.php';?>
+
+</body>
+
+</html>
